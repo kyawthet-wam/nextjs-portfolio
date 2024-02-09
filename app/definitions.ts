7b37ec6a92d15ae544441a5c23bb8b53d4a13e81
kyawthet-wam <kyawthetwam@gmail.com>
2024-02-09 19:45:08 +0630

    ddd

--- app/definitions.ts
+++ app/definitions.ts
@@ -1,16 +1,37 @@
 export type Work = {
-    title:string;
-    place:string;
-    duration:string;
-    link:string;
-}
+  title: string;
+  place: string;
+  duration: string;
+  link: string;
+};
+
+export const works: Work[] = [
+  {
+    title: "Software Engineer Intern",
+    place: "ABC Company",
+    duration: "June 2021 - August 2021",
+    link: "https://www.abccompany.com/careers",
+  },
+  {
+    title: "Frontend Developer",
+    place: "XYZ Tech Solutions",
+    duration: "January 2020 - Present",
+    link: "https://www.xyztechsolutions.com",
+  },
+  {
+    title: "Web Developer",
+    place: "TechStart Innovations",
+    duration: "March 2018 - December 2019",
+    link: "https://www.techstartinnovations.com",
+  },
+];
 
 export type Projects = {
-    title:string;
-    image:string;
-    description:string;
-    playStoreLink?:string;
-    appStoreLink?:string;
-    webLink?:string;
-    photos: string[];
-}
+  title: string;
+  image: string;
+  description: string;
+  playStoreLink?: string;
+  appStoreLink?: string;
+  webLink?: string;
+  photos: string[];
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import { Linkedin, Mail, Phone } from "lucide-react";
+import { CheckCheck, Linkedin, Mail, Phone } from "lucide-react";
+import { works } from "./definitions";
 
 export default function Home() {
   return (
@@ -30,7 +31,7 @@ export default function Home() {
           mobile app projects' success.
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
-          <div className="flex flex-col gap-12">
+          <div className="flex flex-col md:col-span-2 gap-12">
             <div className="flex flex-col gap-2">
               <div className="relative text-md text-black font-bold">
                 Education
@@ -46,6 +47,11 @@ export default function Home() {
               </div>
               <div className="relative text-slate-600 text-sm">
                 I have 3+ years of experience in Software Development
+              </div>
+              <div className="pt-10 space-y-8 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:mx-auto md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent">
+                {works.map((work) => (
+                  <Work />
+                ))}
               </div>
             </div>
           </div>
@@ -79,40 +85,67 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
-        <nav>
-          <div className="grid grid-flow-col gap-10">
-            <a
-              className="hover:text-blue-700 tooltip"
-              data-tip="(+95) 09791867084"
-              href="tel:+959791867084"
-            >
-              <Phone size={24} />
-            </a>
-            <a
-              className="hover:text-blue-700 tooltip"
-              data-tip="kyawthetwam@gmail.com"
-              href="mailto:kyawthetwam@gmail.com"
-            >
-              <Mail size={24} />
-            </a>
-            <a
-              className="hover:text-blue-700 tooltip"
-              data-tip="Kyaw Thet Wam"
-              href="https://www.linkedin.com/in/kyaw-thet-wam-71a0611a2"
-            >
-              <Linkedin size={24} />
-            </a>
-          </div>
-        </nav>
-        <aside>
-          <p>
-            Empowering Ideas, Building Futures | Kyaw Thet Wam © 2024 | Made
-            with Passion and Precision
-          </p>
-        </aside>
-      </footer>
+      <Footer />
     </div>
+  );
+}
+
+export function Work() {
+  return (
+    <div className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active">
+      <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-sky-200 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
+        <CheckCheck color="#0284c7" />
+      </div>
+      <div className="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-slate-200 shadow">
+        <div className="flex items-center justify-between space-x-2 mb-1">
+          <div className="font-bold text-xs text-slate-900">Order Placed</div>
+          <time className="font-sans text-xs text-sky-600">
+            Dec 2019 - Feb 2020
+          </time>
+        </div>
+        <div className="text-xs text-slate-500">
+          Pretium lectus quam id leo. Urna et pharetra aliquam
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function Footer() {
+  return (
+    <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+      <nav>
+        <div className="grid grid-flow-col gap-10">
+          <a
+            className="hover:text-blue-700 tooltip"
+            data-tip="(+95) 09791867084"
+            href="tel:+959791867084"
+          >
+            <Phone size={24} />
+          </a>
+          <a
+            className="hover:text-blue-700 tooltip"
+            data-tip="kyawthetwam@gmail.com"
+            href="mailto:kyawthetwam@gmail.com"
+          >
+            <Mail size={24} />
+          </a>
+          <a
+            className="hover:text-blue-700 tooltip"
+            data-tip="Kyaw Thet Wam"
+            href="https://www.linkedin.com/in/kyaw-thet-wam-71a0611a2"
+          >
+            <Linkedin size={24} />
+          </a>
+        </div>
+      </nav>
+      <aside>
+        <p>
+          Empowering Ideas, Building Futures | Kyaw Thet Wam © 2024 | Made with
+          Passion and Precision
+        </p>
+      </aside>
+    </footer>
   );
 }
 
