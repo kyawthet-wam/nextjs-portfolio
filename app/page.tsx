79d269822430c87a0e50e41b0ce5b658c006037c
kyawthet-wam <kyawthetwam@gmail.com>
2024-02-20 22:15:53 +0630

    single quote fix

--- app/page.tsx
+++ app/page.tsx
@@ -67,7 +67,8 @@ export default function Home() {
               successful project outcomes. My dedication to staying updated with
               industry trends ensures I utilize the latest tools and techniques.
               With a passion for crafting exceptional user experiences, I am
-              committed to contributing to your mobile app projects' success.
+              committed to contributing to your mobile app projects`&apos;`
+              success.
             </div>
           </div>
           <div className="mx-auto">
